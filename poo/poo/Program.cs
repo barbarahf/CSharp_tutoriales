c728996cc29ce9409d744462036f8f3513e707f2
barbarahf <herrerabarbaraf@gmail.com>
2020-09-21 09:11:11 +0200

    poo dotnet

---
+++ poo/poo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace poo
+{
+    class Circulo
+    {
+        private double pi = 3.1416;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- tutorial_dotnet_consol/tutorial_dotnet_consol/colecciones.cs
+++ tutorial_dotnet_consol/tutorial_dotnet_consol/colecciones.cs
@@ -1,13 +1,32 @@
+using System;
 using System.Collections.Generic;
 
 namespace tutorial_dotnet_consol
 {
     public class colecciones
     {
-        static void Main11(string[] args)
+        static void Main(string[] args)
         {
-            List<int> numeros = new List<int>();
-            numeros.Add(5);
+            LinkedList<int> numeros = new LinkedList<int>();
+
+            foreach (int num in new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11})
+            {
+                numeros.AddLast(num);
+            }
+
+            /*
+            foreach (var i in numeros)
+            {
+                Console.Write(i + " ");
+            }*/
+            numeros.Remove(6);
+            LinkedListNode<int> nodoImportante = new LinkedListNode<int>(15);
+            numeros.AddFirst(nodoImportante);
+            for (LinkedListNode<int>? nodo = numeros.First; nodo != null; nodo = nodo.Next)
+            {
+                int numero = nodo.Value;
+                Console.Write(numero + " ");
+            }
         }
     }
 }
