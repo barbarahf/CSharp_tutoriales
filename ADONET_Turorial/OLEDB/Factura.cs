b0bb1d475735ea8843d495dfb077ccc84080c09f
barbarahf <herrerabarbaraf@gmail.com>
2020-11-11 16:32:52 +0100

    Creando un objeto desde una base de datos

---
+++ ADONET_Turorial/OLEDB/Factura.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.SqlTypes;
+
+namespace OLEDB
+{
+    public class Factura
+    {
+        private int _numFactura;
+        private String _nombre;
+        private String _nombreProducto;
+        private int _cantidad;
+        private int _cantidadTotal;
+        public Factura(int numFactura, string nombre, string nombreProducto, int cantidad, int cantidadTotal)
+        {
+            _numFactura = numFactura;
+            _nombre = nombre;
+            _nombreProducto = nombreProducto;
+            _cantidad = cantidad;
+            _cantidadTotal = cantidadTotal;
+        }
+
+        public override string ToString()
+        {
+            return _numFactura+" "+_nombre + " " + _nombreProducto + " " + _cantidad + " " + _cantidadTotal;
+        }
+    }
+}
--- ADONET_Turorial/OLEDB/Program.cs
+++ ADONET_Turorial/OLEDB/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.OleDb;
+using System.Data.SqlTypes;
 
 namespace OLEDB
 {
@@ -7,31 +9,81 @@ namespace OLEDB
     {
         public static void Main(string[] args)
         {
+            List<Factura> facturas = new List<Factura>();
+            //Array de facturas
             string connectionString = "Provider=SQLOLEDB;Server=localhost;Database=Compras; Trusted_Connection=yes;";
+            /*
+             * Obtener un listado de las facturas realizadas indicando numero de factura, nombre del cliente, nombre
+             * del producto, cantidad y precio
+             */
+            string consulta01 =
+                "SELECT numFactura,nombre,nombreprod,cantidad,cantidad*precio FROM ((producto JOIN item_ventas on codiprod=codigoProducto) JOIN ventas ON numFactura=numeroFactura JOIN cliente ON codigo=codigoCliente);";
+
+            crearObjeto(connectionString, consulta01, facturas);
+            foreach (var eFactura in facturas)
+            {
+                Console.WriteLine(eFactura.ToString());
+            }
+        }
+
+        private static void crearObjeto(string connectionString, string insertSql, List<Factura> facturas)
+        {
             using (OleDbConnection connection = new OleDbConnection(connectionString))
             {
-                string consultas =
-                    "SELECT numFactura,nombre,nombreprod,cantidad,cantidad*precio FROM ((producto JOIN item_ventas on codiprod=codigoProducto) JOIN ventas ON numFactura=numeroFactura JOIN cliente ON codigo=codigoCliente);";
+                OleDbCommand command = new OleDbCommand(insertSql);
+
+                command.Connection = connection;
 
                 try
                 {
-                    OleDbCommand command = new OleDbCommand(consultas, connection);
-
                     connection.Open();
-
                     OleDbDataReader reader = command.ExecuteReader();
 
 
-                    while (reader.Read())
+                    while (reader.Read()) //cada columnna
+                    {
+                        int numFactura = (int) reader[0];
+                        String nombre = reader[1].ToString();
+                        string nombreProducto = reader[2].ToString();
+                        int cantidad = (int) reader[3];
+                        String cantidadTotal = reader[4].ToString();
+                        facturas.Add(new Factura(numFactura, nombre, nombreProducto, cantidad,
+                            Int16.Parse(cantidadTotal)));
+                    }
+
+                    reader.Close();
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private static void Consulta(string connectionString, string insertSql)
+        {
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                OleDbCommand command = new OleDbCommand(insertSql);
+
+                command.Connection = connection;
+
+                try
+                {
+                    connection.Open();
+                    OleDbDataReader reader = command.ExecuteReader();
+
+
+                    while (reader.Read()) //cada columnna
                     {
                         for (int i = 0; i < reader.FieldCount; i++)
                         {
-                            Console.Write(reader[i].ToString() + " ");
+                            Console.Write(reader[i] + " ");
                         }
 
                         Console.WriteLine();
                     }
-
 
                     reader.Close();
                     connection.Close();
@@ -44,6 +96,3 @@ namespace OLEDB
         }
     }
 }
-// Console.WriteLine("Conected");
-// Console.WriteLine("ServerVersion: {0} \nDataSource: {1}", connection.ServerVersion,
-//     connection.DataSource);
