9f56fd974cbeeebbd0998db7342c1fa0ee61b991
barbarahf <herrerabarbaraf@gmail.com>
2020-11-11 15:34:25 +0100

    oledb consultas

--- ADONET_Turorial/OLEDB/Program.cs
+++ ADONET_Turorial/OLEDB/Program.cs
@@ -6,18 +6,35 @@ namespace OLEDB
     internal class Program
     {
         public static void Main(string[] args)
-        { 
-           
-            string connectionString = "Provider=SQLOLEDB;Server=localhost;Database=store; Trusted_Connection=yes;";
+        {
+            string connectionString = "Provider=SQLOLEDB;Server=localhost;Database=Compras; Trusted_Connection=yes;";
             using (OleDbConnection connection = new OleDbConnection(connectionString))
             {
+                string consultas =
+                    "SELECT numFactura,nombre,nombreprod,cantidad,cantidad*precio FROM ((producto JOIN item_ventas on codiprod=codigoProducto) JOIN ventas ON numFactura=numeroFactura JOIN cliente ON codigo=codigoCliente);";
+
                 try
                 {
+                    OleDbCommand command = new OleDbCommand(consultas, connection);
+
                     connection.Open();
-                    Console.WriteLine("Conected");
-                    Console.WriteLine("ServerVersion: {0} \nDataSource: {1}",
-                        connection.ServerVersion, connection.DataSource);
-                     connection.Close();
+
+                    OleDbDataReader reader = command.ExecuteReader();
+
+
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.Write(reader[i].ToString() + " ");
+                        }
+
+                        Console.WriteLine();
+                    }
+
+
+                    reader.Close();
+                    connection.Close();
                 }
                 catch (Exception ex)
                 {
@@ -27,3 +44,6 @@ namespace OLEDB
         }
     }
 }
+// Console.WriteLine("Conected");
+// Console.WriteLine("ServerVersion: {0} \nDataSource: {1}", connection.ServerVersion,
+//     connection.DataSource);
